026a6f75c9358e652f26db5c7f92daaf474ad26e
Fede Álvarez <fedealvarezcampos@gmail.com>
2022-01-17 20:25:27 +0100

    Custom routes

--- src/api/newsitem/routes/custom-newsitem.js
+++ src/api/newsitem/routes/custom-newsitem.js
@@ -10,5 +10,26 @@ module.exports = {
 				},
 			},
 		},
+		{
+			method: 'POST',
+			path: '/newsitems/:id/likes',
+			handler: 'newsitem.like',
+			config: {
+				find: {
+					auth: true,
+				},
+			},
+		},
 	],
 };
+
+// {
+// 	method: 'GET',
+// 	path: '/newsitems/:id/likes',
+// 	handler: 'newsitem.likes',
+// 	config: {
+// 		find: {
+// 			auth: false,
+// 		},
+// 	},
+// },
