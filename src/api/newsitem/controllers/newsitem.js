21ce3af91eefbf6b38d0f0d51d7b1cb356f241a0
Fede Álvarez <fedealvarezcampos@gmail.com>
2022-01-17 20:26:02 +0100

    Customize services and controllers

--- src/api/newsitem/controllers/newsitem.js
+++ src/api/newsitem/controllers/newsitem.js
@@ -1,29 +1,70 @@
 'use strict';
 
-/**
- *  newsitem controller
- */
-
 const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::newsitem.newsitem', ({ strapi }) => ({
-	// Method 1: Creating an entirely custom action
+	// async find(ctx) {
+	// 	const { query } = ctx;
+
+	// 	const entity = await strapi.service('api::newsitem.newsitem').find(query);
+
+	// 	const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+	// 	return this.transformResponse(sanitizedEntity);
+	// },
+
 	async likes(ctx) {
 		try {
-			const { id } = ctx.params;
+			const { id: postID } = ctx.params;
 
-			const entity = await strapi.entityService.findOne('api::newsitem.newsitem', id, {
-				fields: ['id'],
-				populate: {
-					users: {
-						fields: ['id'],
-					},
+			const user = ctx.state.user ?? null;
+
+			const existingUserIDs = await strapi
+				.service('api::newsitem.newsitem')
+				.getAlreadyExistingUserIDs(postID);
+
+			const isLiked = existingUserIDs?.includes(user?.id) ?? false;
+
+			return this.transformResponse({ id: postID, isLiked: isLiked });
+		} catch (err) {
+			ctx.body = err;
+		}
+	},
+
+	async like(ctx) {
+		try {
+			const { id: postID } = ctx.params;
+			const { id: userID } = ctx.state.user;
+
+			const existingUserIDs = await strapi
+				.service('api::newsitem.newsitem')
+				.getAlreadyExistingUserIDs(postID);
+
+			let newUserIDs = [];
+
+			if (existingUserIDs?.includes(userID)) {
+				newUserIDs = existingUserIDs.filter(id => id !== userID);
+			} else {
+				newUserIDs = [...existingUserIDs, userID];
+			}
+
+			console.log(newUserIDs);
+
+			const entity = await strapi.entityService.update('api::newsitem.newsitem', postID, {
+				data: {
+					users: newUserIDs,
 				},
+				populate: '*',
 			});
 
 			const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
 
-			return this.transformResponse({ likes: sanitizedEntity?.users?.length });
+			console.log(sanitizedEntity.users);
+
+			return this.transformResponse({
+				postID: sanitizedEntity?.id,
+				likes: sanitizedEntity?.users?.length,
+			});
 		} catch (err) {
 			ctx.body = err;
 		}
--- src/api/newsitem/services/newsitem.js
+++ src/api/newsitem/services/newsitem.js
@@ -6,4 +6,38 @@
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::newsitem.newsitem');
+module.exports = createCoreService('api::newsitem.newsitem', ({ strapi }) => ({
+	async find(...args) {
+		const { results, pagination } = await super.find(...args);
+
+		const entries = await strapi.entityService.findMany('api::newsitem.newsitem', {
+			fields: ['title'],
+			populate: {
+				users: {
+					id: true,
+				},
+			},
+		});
+
+		results.map((e, i) => (e.likes = entries[i].users.length));
+
+		return { results, pagination };
+	},
+
+	async getAlreadyExistingUserIDs(postID) {
+		const existingUsers = await strapi.entityService.findOne('api::newsitem.newsitem', postID, {
+			fields: ['title'],
+			populate: '*',
+		});
+
+		const usersThatAlreadyLiked = existingUsers?.users;
+
+		let existingUserIDs = [];
+
+		for (const user of usersThatAlreadyLiked) {
+			existingUserIDs.push(user?.id);
+		}
+
+		return existingUserIDs;
+	},
+}));
